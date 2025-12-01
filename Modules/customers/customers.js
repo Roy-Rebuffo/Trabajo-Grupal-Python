72ab6becc3bb71c84f2c074ab9bc7dc50006d2e9
Fabianlv27 <fabianlv1920@gmail.com>
2025-12-01 20:06:49 +0100

    Se ha listado los clientes en html

--- Modules/customers/customers.js
+++ Modules/customers/customers.js
@@ -10,9 +10,48 @@ function collectCustomerData() {
 }
 
 function SentData() {
-    pywebview.api.customers_GetCustomers()
-  .then(data => console.log("OK:", data))
-  .catch(err => console.error("ERROR:", err));
     collectCustomerData();
+
     pywebview.api.customers_AddCustomer(customerData)
 }
+
+
+
+window.addEventListener('pywebviewready', function () {
+
+    console.log("pywebview:", pywebview);
+    console.log("apis disponibles:", Object.keys(pywebview.api));
+
+    listarClientes();
+},{once: true});
+
+function listarClientes() {
+    pywebview.api.customers_GetCustomers().then((data) => {
+        let customerList = document.getElementById('customerList');
+        customerList.innerHTML = '';
+
+        Object.keys(data).forEach(key => {
+            const customer = data[key];
+
+            let listItem = document.createElement('li');
+
+            listItem.innerHTML = `
+                <div class="customer-card">
+                    <h3>${customer.name} ${customer.surname}</h3>
+                    <div class="customer-details">
+                    <p><strong>Edad:</strong> ${customer.age}</p>
+                    <p><strong>Ciudad:</strong> ${customer.city}</p>
+                    <p><strong>Email:</strong> ${customer.email}</p>
+                    <p><strong>Teléfono:</strong> ${customer.phone}</p>
+                    <p><strong>Activo:</strong> ${customer.active ? "Sí" : "No"}</p>
+                    <p><strong>Cantidad de compras:</strong> ${customer.pucharses_amount}</p>
+                    <p><strong>Total gastado:</strong> ${customer.total_spent}€</p>
+                    </div>
+                </div>
+            `;
+
+            customerList.appendChild(listItem);
+        });
+    });
+}
+
