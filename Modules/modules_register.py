ba0e1a630da1976625d5fa7f1868bcc3362c37b3
Rafael <gonzalesstalin05@gmail.com>
2025-12-02 01:57:47 +0100

    Avances en módulo de usuarios con roles y guardado en JSON

--- Modules/modules_register.py
+++ Modules/modules_register.py
@@ -1,9 +1,11 @@
 from Modules.customers.M_Customers import M_Customers
-
+from Modules.usuarios.M_users import M_users
 clientes=M_Customers()
+usuarios = M_users()
 
 modules={
     
-    "customers":clientes
+    "customers":clientes,
+    "users":usuarios
     
     }
