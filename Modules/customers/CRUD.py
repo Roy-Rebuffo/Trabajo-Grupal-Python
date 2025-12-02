3b9a450797bef3e5f106aa640fca261d5524344d
Fabianlv27 <fabianlv1920@gmail.com>
2025-12-02 16:14:16 +0100

    Añadiendo los filtros finales en Customers

---
+++ Modules/customers/CRUD.py
@@ -0,0 +1,74 @@
+import json
+from pprint import pprint
+import uuid
+
+class Methods:
+
+    @staticmethod
+    def GetAll(filepath):
+        try:
+            with open(filepath, 'r') as fich:
+                data = json.load(fich)
+                return data
+        except FileNotFoundError as error:
+            print("Error:", error)
+
+    @staticmethod
+    def AddMany(filepath, element):
+        data = Methods.GetAll(filepath)
+        data.update(element)
+        print(data)
+        try:
+            with open(filepath, 'w') as fich:
+                json.dump(data, fich, indent=4)
+                print("Datos guardados correctamente.")
+        except Exception as error:
+            print("Error al guardar los datos:", error)
+
+    @staticmethod
+    def Delete(filepath, element_id):
+        data = Methods.GetAll(filepath)
+        if element_id in data:
+            del data[element_id]
+            try:
+                with open(filepath, 'w') as fich:
+                    json.dump(data, fich, indent=4)
+                    print(f"Elemento con ID {element_id} eliminado correctamente.")
+            except Exception as error:
+                print("Error al eliminar el elemento:", error)
+        else:
+            print(f"No se encontró el elemento con ID {element_id}.")
+
+    @staticmethod
+    def Update(filepath, element_id, new_data):
+        data = Methods.GetAll(filepath)
+        if element_id in data:
+            data[element_id].update(new_data)
+            try:
+                with open(filepath, 'w') as fich:
+                    json.dump(data, fich, indent=4)
+                    print(f"Elemento con ID {element_id} actualizado correctamente.")
+            except Exception as error:
+                print("Error al actualizar el elemento:", error)
+        else:
+            print(f"No se encontró el elemento con ID {element_id}.")
+
+    @staticmethod
+    def AddOne(filepath, customer_data):
+        data = Methods.GetAll(filepath)
+        element_id = str(uuid.uuid4())
+        if element_id not in data:
+            data[element_id] = customer_data
+            try:
+                with open(filepath, 'w') as fich:
+                    json.dump(data, fich, indent=4)
+                    print(f"Elemento con ID {element_id} añadido correctamente.")
+            except Exception as error:
+                print("Error al añadir el elemento:", error)
+        else:
+            print(f"El elemento con ID {element_id} ya existe.")
+
+    @staticmethod
+    def GetOneById(filepath, element_id):
+        data = Methods.GetAll(filepath)
+        return data.get(element_id, None)
--- Modules/customers/M_Customers.py
+++ Modules/customers/M_Customers.py
@@ -1,6 +1,18 @@
-
-from Functions.CRUD import Methods
+import operator
+from datetime import datetime
+import pprint
+from CRUD import Methods
 from collections import defaultdict
+
+OPERADORES = {
+    "==": operator.eq,
+    "!=": operator.ne,
+    ">":  operator.gt,
+    "<":  operator.lt,
+    ">=": operator.ge,
+    "<=": operator.le
+}
+
 
 class M_Customers:
     #CRUD Basico
@@ -24,8 +36,68 @@ class M_Customers:
 
     #CRUD Avanzado
 
+    def Filter_customers(self, criterios,restrition,customers):
+        """
+        Filtra los clientes según un criterio y valor dados.
+        criterio: [{}] -> lista de diccionarios con clave: ,
+        claves del diccionario:
+        key_name: nombre de la clave interna a filtrar
+        value: valor a comparar
+        comparador: tipo de comparación (==, !=, >, <, >=, <=, between)
+        """
+        for criterio in criterios:
+            key_name = criterio['key_name']
+            comparador = criterio['comparador']
+            valor = criterio['valor']
 
-    def GetFilterCustomers(self,datos,asc, ordenar_por=None, agrupar_por=None):
+            if key_name not in customers[next(iter(customers))]:
+                continue  # Salta si la clave no existe
+
+            tipo_dato = type(customers[next(iter(customers))][key_name])
+            if 'type' in criterio:
+                if criterio['type'] == 'date':
+                    tipo_dato = datetime
+            print(f"Tipo de dato para {key_name}: {tipo_dato}")
+            if tipo_dato == bool:
+                valor = bool(valor)
+            elif tipo_dato == int or tipo_dato == float:
+                valor = float(valor)
+            elif tipo_dato == str:
+                valor = str(valor)
+            elif tipo_dato == datetime:
+                if comparador == 'between':
+                    valor = [datetime.strptime(v, "%Y-%m-%d") for v in valor]
+                else:
+                    valor = datetime.strptime(valor, "%Y-%m-%d")
+
+            operador_func = OPERADORES.get(comparador)
+
+            if not operador_func:
+                continue  # Salta si el comparador no es válido
+
+            clientes_filtrados = {}
+            for clave, info in customers.items():
+                dato_cliente = info[key_name]
+                if tipo_dato == datetime:
+                    dato_cliente = datetime.strptime(dato_cliente, "%Y-%m-%d")
+
+                if comparador == 'between' and tipo_dato == datetime:
+                    if valor[0] <= dato_cliente <= valor[1]:
+                        clientes_filtrados[clave] = info
+                else:
+                    if not operador_func(dato_cliente, valor):
+                        if clave in clientes_filtrados and restrition==True:
+                            del clientes_filtrados[clave] 
+                        continue
+                    else:
+                        clientes_filtrados[clave] = info
+                    
+
+            customers = clientes_filtrados
+        return customers
+             
+
+    def Get_sorted_and_grouped_customers(self,datos,asc, ordenar_por=None, agrupar_por=None):
         """
         datos: dict -> tu diccionario de clientes
         ordenar_por: str o list[str] -> claves internas por las que ordenar
@@ -55,3 +127,14 @@ class M_Customers:
 
         # Si no hay agrupamiento, devuelvo dict ordenado
         return dict(datos_ordenados)
+
+if __name__ == "__main__":
+    mc = M_Customers()
+    clientes = mc.GetCustomers()
+    criterios = [
+        {"key_name": "age", "comparador": ">", "valor": 30},
+        {"key_name": "active", "comparador": "==", "valor": True},
+        {"key_name": "date", "comparador": "between", "valor": [ "1988-01-01","1989-01-01"],"type": "date"}
+    ]
+    filtrados = mc.Filter_customers(criterios,True, clientes)
+    print("Filtrados:", filtrados)
