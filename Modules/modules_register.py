3770e9511b754dc7a5379270898a7a14d478d3ae
Fabianlv27 <fabianlv1920@gmail.com>
2025-12-04 21:09:45 +0100

    Fabian creo sus consultas

--- Modules/modules_register.py
+++ Modules/modules_register.py
@@ -3,13 +3,14 @@ from Modules.services.M_Services import M_Services
 from Modules.products.M_Products import M_Products
 from Modules.Facturas.M_Facturas import M_Facturas
 from Modules.usuarios.M_users import M_users
+from Modules.Cuestiones_Fabian.M_Cuestiones_Fabian import M_cuestiones_Fabian
 
 clientes=M_Customers()
 usuarios = M_users()
 servicios=M_Services()
 productos=M_Products()
 facturas = M_Facturas()
-
+M_cuestiones_Fabian_instance=M_cuestiones_Fabian()
 
 modules={
     
@@ -17,7 +18,8 @@ modules={
     "users":usuarios,
     "products" :productos,
     "services":servicios,
-    "Facturas":facturas
+    "Facturas":facturas,
+    "C_Fabian" :M_cuestiones_Fabian_instance
     }
 
     
